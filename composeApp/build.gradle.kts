408f1847c3f28f1f2922bf50d812b6833521893a
sam-sudo <samuelhueso19@gmail.com>
2024-04-23 20:31:47 +0200

    Voyage library added

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -36,6 +36,9 @@ kotlin {
             implementation(libs.androidx.activity.compose)
         }
         commonMain.dependencies {
+            implementation(libs.voyager.navigator)
+            implementation(libs.voyager.transitions)
+            implementation(libs.voyager.tabNavigator)
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material)
